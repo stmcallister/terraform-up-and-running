28bb2ca14f7127eba9a7620b3770106282836c40
Scott McAllister <smcallister@pagerduty.com>
2020-01-17 12:12:44 -0800

    adding chapter folders and chapter3/

---
+++ chapter3/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "smcallister-terraform-up-and-running-state"
+
+  # prevent accidental deletion of s3 bucket
+  lifecycle {
+      prevent_destroy = true
+  }
+
+  # Enable versioning to see full revision history
+  versioning {
+      enabled = true
+  }
+
+  # Enable server-side encryption defaultt
+  server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+          }
+      }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+      name = "LockID"
+      type = "S"
+  }
+}
